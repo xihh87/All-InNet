d3fc33a40a6b979d8dcd2a3d32552e82bf45f788
Ollin Demian <ollin.langle@ciencias.unam.mx>
2016-11-25 14:50:12 -0600

    Sin menú

---
+++ Solitos/NBM_comunities.jl
@@ -0,0 +1,90 @@
+using LightGraphs
+using RCall
+
+
+red = readdlm("zacharykarateclub.dat")
+g = Graph()
+ultimovertice = Int64(maximum(red))
+add_vertices!(g,ultimovertice)
+for n in 1:Int64((length(red)/2))
+    add_edge!(g,Int64(red[n,1]),Int64(red[n,2]))
+end
+
+function la_matriz(g)
+    A = full(adjacency_matrix(g))
+    ceros = zeros(A)
+    D = zeros(A)
+    menos = -1 * eye(A)
+    for n in 1:nv(g)
+        D[n,n] = degree(g,n)-1
+    end
+    sparse(hcat(vcat(ceros,menos),vcat(D,A)))
+end
+
+M = la_matriz(g)
+valores, vectores = eigs(M, nev=20)
+treshold = real(sqrt(valores[1]))
+if real(last(valores)) > treshold
+    valores, vectores = eigs(M, nev=30)
+    if real(last(valores)) > treshold
+        valores, vectores = eigs(M, nev=40)
+        if real(last(valores)) > treshold
+            valores, vectores = eigs(M, nev=50)
+            if real(last(valores)) > treshold
+                valores, vectores = eigs(M, nev=nv(g))
+            end
+        end
+    end
+end
+
+
+cuantos = Array(Float64,0)
+index = Array(Int64,0)
+for i in 1:length(valores)
+    if imag(valores[i]) == 0 && real(valores[i]) > treshold
+        push!(cuantos,real(valores[i]))
+        push!(index,i)
+    end
+end
+
+print("There are $(length(cuantos)) communities in this Network \n")
+
+matriz_embedded = real(vectores[:,index])
+arriba = matriz_embedded[1:nv(g),:]
+abajo = matriz_embedded[nv(g)+1:2*nv(g),:]
+hola = arriba + abajo
+
+R_kmeans = R"kmeans";
+
+function membresia(n,v)
+    membresia = Array(Int64,0)
+    if n == 2
+        for i in 1:length(v[:,1])
+            if sign(v[i,2]) == sign(1)
+                push!(membresia,1)
+            else
+                push!(membresia,2)
+            end
+        end
+    else
+        matriz_sirve = v
+        cluster_p = R_kmeans(matriz_sirve[:,2:n-1],n,nstart=500)
+        for i in 1:length(cluster_p[1])
+            push!(membresia,cluster_p[1][i])
+        end
+    end
+    membresia
+end
+
+grupos = membresia(length(index),hola)
+
+if typeof(red[1,1]) != Int
+  comunidades = hcat(Nodes,grupos)
+else
+  comunidades = hcat(vertices(g),grupos)
+end
+
+print("Your results will be saved as: NBM_communities_$(a[k]) \n")
+writedlm("../Resultados/NBM_communities_$(a[k])",comunidades)
+
+
